d6bfd6f05451cbf0133f0be31b84616a4ea102ef
Oleksii Kuznietsov <okuznetsov@flexservice.com>
2020-01-02 14:24:10 +0200

    add

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,47 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-    currentResult = currentResult + +userInput.value;
-    outputResult(currentResult, '');
+function getUserNumberInput () {
+    return parseInt(usrInput.value);
+}
+
+function createAndWriteOutput (operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
+function add () {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function subtract () {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply () {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide () {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 // currentResult = add(1, 2);
 //
